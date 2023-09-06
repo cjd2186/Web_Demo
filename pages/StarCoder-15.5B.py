3ec1b85899da653c5ef3a91d3decda6ea82f9082
Jonna's MacBook <chrisd456@Jonnass-MacBook-Pro.local>
2023-09-06 01:47:38 -0400

    working prototype of examples pass/fail, need to refine to ensure correct distribution of tests and classifcation of examples as being passed or failed, code is symmetricl for pass and fail

--- pages/StarCoder-15.5B.py
+++ pages/StarCoder-15.5B.py
@@ -11,17 +11,17 @@ def main():
         df = pd.DataFrame.from_dict(pd.json_normalize(data), orient='columns')
         cols = df.columns.tolist()
 
-    mbpp_df = {}
+    mbpp_dict = {}
     #Load in JSONL file (generated functions)
     with open("/Users/chrisd456/Documents/Columbia Work/Summer 2023/Yale_Research/Web_Demo/predictions_step_0_rank_0.jsonl") as f:
 
         for line in f:
             data = json.loads(line)
             for key in data.keys():
-                if key not in mbpp_df:
-                    mbpp_df[key] = [data[key]]
+                if key not in mbpp_dict:
+                    mbpp_dict[key] = [data[key]]
                 else:
-                    mbpp_df[key].append(data[key])
+                    mbpp_dict[key].append(data[key])
 
     st.set_page_config(
         page_title="NLP Paper", 
@@ -42,7 +42,7 @@ def main():
     if choice == cols[4]:
         gsm8k8()
     if choice == cols[5]:
-        mbpp3(mbpp_df)
+        mbpp3(mbpp_dict)
     if choice == cols[6]:
         mwr()
 
@@ -55,29 +55,110 @@ def wikitq2():
 def gsm8k8():
     return None
 
-def mbpp3(mbpp_df):
-    """tests=['passed tests only', 'failed tests only', 'all examples']
+def mbpp3(mbpp_dict):
+    tests=['passed tests only', 'failed tests only', 'all examples']
     choice=st.sidebar.radio('Select Dataset Examples:', tests)
     
     passed=True
     failed=True
 
-    if choice== tests[0]:
+    x=mbpp_dict["generated_program"]
+    y=mbpp_dict['metadata']
+
+
+    passed_tests= mbpp_dict
+    failed_tests= mbpp_dict
+
+    assertions=[]
+    results= []
+    if choice == tests[0]:
         passed=True
         failed=False
+        st.write("PASSED")
+
+        i=0 
+        length=len(passed_tests['metadata'])
+        #iterate through the function examples
+        while i < length:
+            #determine which assertions to display, assume all passed
+            tests=[1, 1, 1]
+
+            #iterate through list of tests cases in exec_result
+            exec_acc= x[i]["exec_acc"]
+            
+            #see if test failed, take out of passed tests dict
+            #only want passed_tests to have function information for passed tests
+            if (exec_acc == 0):
+                del (passed_tests["generated_program"][i])
+                del (passed_tests["metadata"][i])
+            
+            else:
+                #compute which test cases were passed, want to show which assertions passed too
+                for j in range(0,3):
+                    result= x[i]["exec_result"][j]["result"]
+                    #check for failed tests
+                    if (result != "passed"):
+                        tests[j] = 0  
+                                      
+                
+                    #add passed result and assertion
+                    if (tests[j]== 1):
+                        assertion= y[i]["test_list"][j]
+                        result= x[i]["exec_result"][j]["result"]
+                        assertions.append(assertion)
+                        results.append(result)
+            
+            length = len(passed_tests['metadata'])
+            i+=1
+
+
+
     if choice== tests[1]:
         passed=False
-        failed=True"""
-    
-    y=mbpp_df['metadata']
-    x=mbpp_df["generated_program"]
+        failed=True
+        st.write("FAILED")
+
+        i=0 
+        length=len(failed_tests['metadata'])
+        #iterate through the function examples
+        while i < length:
+            #determine which assertions to display, assume all failed
+            tests=[0, 0, 0]
+
+            #iterate through list of tests cases in exec_result
+            exec_acc= x[i]["exec_acc"]
+            
+            #see if test failed, take out of passed tests dict
+            #only want failed to have function information for failed tests
+            if (exec_acc == 1):
+                del (failed_tests["generated_program"][i])
+                del (failed_tests["metadata"][i])
+            
+            else:
+                #compute which test cases were failed, want to show which assertions failed too
+                for j in range(0,3):
+                    result= x[i]["exec_result"][j]["result"]                                    
+                
+                    #check for failed tests
+                    if (result == "passed"):
+                        tests[j] = 1
+
+                    #add failed result and assertion
+                    if (tests[j]== 0):
+                        assertion= y[i]["test_list"][j]
+                        result= x[i]["exec_result"][j]["result"]
+                        assertions.append(assertion)
+                        results.append(result)
+            
+            length = len(failed_tests['metadata'])
+            i+=1
 
     functions=[]
     prpts=[]
     func_sigs=[]
     
     #list of text and program as a string
-    for i in range (0, len(mbpp_df['metadata'])):
+    for i in range (0, len(mbpp_dict['metadata'])):
         z="# "+ y[i]["text"]
         z+="\n"+ x[i]["program"]
         prpt= y[i]["prompt"]
@@ -85,12 +166,19 @@ def mbpp3(mbpp_df):
         func_sigs.append(func_sig)
         functions.append(z) 
         prpts.append(prpt)
+        if (choice == tests[2]):
+            for j in range(0,3):
+                assertion= y[i]["test_list"][j]
+                result= x[i]["exec_result"][j]["result"]
+                assertions.append(assertion)
+                results.append(result)
 
     #list of test cases, and if they were passed
     
     current_page = st.session_state.get("current_page", 1)
     results_ppg=10
-    total_pgs = (len(functions) + results_ppg - 1) // results_ppg
+    #total_pgs = (len(functions) + results_ppg - 1) // results_ppg
+    total_pgs = ((len(results))//3 + results_ppg - 1) // results_ppg
 
 
     #pagination navigation
@@ -112,7 +200,10 @@ def mbpp3(mbpp_df):
     #display function definitions and prompts
     prmpt_funcs=[functions[start:end], prpts[start:end]]
     sigs=func_sigs[start:end]  
+    tests_results= [assertions[start:end], results[start:end]]
+
     for i in range(0, len(functions[start:end])):
+    #for i in range(0, len(tests_results[start:end])):
         st.markdown(f"### Function {10*(current_page -1) + i}")
         with st.expander(f":red[Function {sigs[i]}]", expanded=True):
             st.code(body=prmpt_funcs[0][i],
@@ -120,7 +211,15 @@ def mbpp3(mbpp_df):
         with st.expander(f":blue[Prompt {sigs[i]}]"):
             st.code(body=prmpt_funcs[1][i],
                     language= "python")
-        
+        with st.expander(":violet[Tests and Results]"):
+            for i in range(0, 3):
+                st.markdown(f":green[Test] {i}")
+                st.code(body=tests_results[0][i],
+                        language="python")
+                st.markdown(f":orange[Result] {i}")
+                st.code(body=tests_results[1][i],
+                        language="python")
+            
     st.session_state.current_page = current_page
 
 def mwr():
